2d1c70fb789a20af0a619ea073a1bd533d5486e2
Dmitriy Malayev <56614284+DmitriyMalayev@users.noreply.github.com>
2021-11-21 15:10:02 -0500

    middleware and favicon

--- express_app/index.js
+++ express_app/index.js
@@ -1,4 +1,6 @@
 import express from "express";
+import favicon from "serve-favicon";
+import path from "path";
 import data from "./data/data.json";
 
 const app = express();
@@ -6,21 +8,43 @@ const PORT = 3000;
 
 //This is for the public folder on path "/"
 app.use(express.static("public"));
+// method to use JSON.
+// Allows us to use JSON in our data on our server
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
 
 //This is for the images folder on path "/images"
 app.use("./images", express.static("images"));
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
 
 app.get("/", (req, res) => {
   res.json(data);
 });
 
+// {"hello": "This is an example of JSON data"}
+// hello=URLEncoded+This+is+an+example+of+URLEncoded+Data
+
+app.post("/newItem", (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
+});
+
 app.get(
   "/item/:id",
   (req, res, next) => {
+    // This is the middleware that pulls the data
     console.log(req.params.id, "req.params.id");
     let user = Number(req.params.id);
     console.log(user);
     console.log(data[user]);
+    // Middleware that uses the request object.
+    // MIddleware are functions that are happening before we are sending a response back.
+
+    console.log(`Request from: ${req.originalUrl}`);
+    console.log(`Request type: ${req.method}`);
+    // EVERYTHING ABOVE THIS LINE IS MIDDLEWARE (JUST BEFORE THE RESPONSE)
+
     res.send(data[user]); //There can only be one response
     next(); //There can be multiple next() callbacks
   },
@@ -49,11 +73,6 @@ app.get("/images", (req, res) => {
   res.download("images/rocket.jpg");
 });
 
-app.listen(PORT, () => {
-  console.log(`Your server is running on port ${PORT}`);
-  console.log(data);
-});
-
 //Chaining Example
 
 app.get("/item", (req, res) => {
@@ -74,6 +93,26 @@ app
     res.send(`A delete request with /item route on port ${PORT}`);
   });
 
+// Error Handling
+// Express automatically handles errors unless we want to customize it.
+// If we want to use Error Handling Middleware it needs to be placed last.
+app.route("/item");
+get((req, res) => {
+  throw new Error();
+});
+
+//Error Handling Function
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send(`Red Alert! Red Alert: ${err.stack}`);
+});
+
+//Place Error Handling Middleware before app.listen
+app.listen(PORT, () => {
+  console.log(`Your server is running on port ${PORT}`);
+  console.log(data);
+});
+
 /*
 index.js is the entry point of our project
 
@@ -81,6 +120,10 @@ How to load static files inside of our server
 
 express.static
   Provides ability to add static files to our server
+express.json
+  Allows us to parse JSON POST comamnds. 
+  When a form or the front end wants to send new data to our server and DB, and send it in JSON format, 
+  we need our server to be able to use it in this format and then post it to the database.
 app.use
   This is a method in express  
   Allows us to add a specific middleware to a path
@@ -92,4 +135,7 @@ next()
   Allows to do multiple callbacks 
   Allows us to create our own middleware to do anything we want before or after
   You can only have one response method for a single call.
+middleware
+  In the context of node express server
+  Functions that have access to the request and response object in our Express Application and can run their code there
 */
